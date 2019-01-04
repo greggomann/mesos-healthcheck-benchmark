484c4af06cd0e8f31e0d743186252ba4750c55dc
Greg Mann <gregorywmann@gmail.com>
2019-01-04 15:17:44 -0800

    Update healthcheck response script.

--- healthcheck-response.py
+++ healthcheck-response.py
@@ -1,6 +1,7 @@
 #!/opt/mesosphere/bin/python
 
 import datetime
+import math
 import sys
 
 start_times = {}
@@ -16,11 +17,10 @@ for line in sys.stdin:
     start_time = datetime.time(hour=int(time_split[0]), minute=int(time_split[1]), second=int(second_split[0]), microsecond=int(second_split[1]))
 
     dot_separated = line.split('.')
-
     container_id = dot_separated[7].replace("'\n", '')
 
     start_times[container_id] = start_time
-  elif 'FETCHING to RUNNING' in line:
+  elif 'FETCHING to RUNNING' in line and 'check-' in line:
     space_separated = line.split(' ')
 
     timestamp = space_separated[6]
@@ -41,4 +41,21 @@ for container_id in start_times.keys():
     time_delta = datetime.datetime.combine(date, finish_times[container_id]) - datetime.datetime.combine(date, start_times[container_id])
     response_times.append(time_delta.total_seconds())
 
+print('Response times:')
 print(response_times)
+
+print('\nN = {}'.format(len(response_times)))
+
+total = 0.
+for duration in response_times:
+  total += duration
+
+mean = total / len(response_times)
+
+print('\nMean: {}'.format(mean))
+
+total = 0.
+for duration in response_times:
+  total += (duration - mean)**2
+
+print('\nStandard deviation: {}'.format(math.sqrt(total / len(response_times))))
