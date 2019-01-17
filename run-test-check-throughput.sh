a71aa5cb36d7cee3d26d67db8762047ab5a187f8
Greg Mann <gregorywmann@gmail.com>
2019-01-16 16:44:28 -0800

    Update the test runner script.

--- run-test-check-throughput.sh
+++ run-test-check-throughput.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Usage: ./run-test-check-throughput.py [master_ip:port] [agent_ip] [array of num_tasks, i.e. '1 2 4 8']
+# Usage: ./run-test-check-throughput.sh [master_ip:port] [agent_ip] [array of num_tasks, i.e. '1 2 4 8']
 
 ARGUMENTS=("$@")
 NUM_ARGS=${#ARGUMENTS[@]}
@@ -30,14 +30,28 @@ for (( i = 2; i < $NUM_ARGS; i++ )); do
     sleep 60
     printf "... " >&3
   done
-  printf "\n" >&3
 
-  LOG_FILENAME="check-throughput-agent-$NUM_TASKS.txt"
+  printf "generating logs ... " >&3
+  LOG_FILENAME="agent-log.txt"
   ssh $AGENT_ADDRESS "journalctl -u dcos-mesos-slave --since='5 minutes ago' > $LOG_FILENAME"
-  scp $AGENT_ADDRESS:~/$LOG_FILENAME ./
 
-  kill $SCHEDULER_PID
+  printf "running perf --no-inherit ... " >&3
+  ssh -t $AGENT_ADDRESS 'sudo perf record --output=/home/centos/perf-no-inherit.data --freq=100 --no-inherit --call-graph dwarf -p `ps aux | grep mesos-agent | grep packages | grep -v grep | awk "{ print \\\$2 }"` -- sleep 60'
+  ssh -t $AGENT_ADDRESS 'sudo chown centos:centos /home/centos/perf-no-inherit.data'
+  printf "running perf ..." >&3
+  ssh -t $AGENT_ADDRESS 'sudo perf record --output=/home/centos/perf-inherit.data --freq=100 --call-graph dwarf -p `ps aux | grep mesos-agent | grep packages | grep -v grep | awk "{ print \\\$2 }"` -- sleep 60'
+  ssh -t $AGENT_ADDRESS 'sudo chown centos:centos /home/centos/perf-inherit.data'
+
+  printf "creating test result artifact ..." >&3
+  TARBALL_FILENAME="check-throughput-agent-results-$NUM_TASKS.tgz"
+
+  ssh $AGENT_ADDRESS "tar -cvzf check-throughput-agent-results-$NUM_TASKS.tgz perf-no-inherit.data perf-inherit.data $LOG_FILENAME"
+  scp $AGENT_ADDRESS:~/check-throughput-agent-results-$NUM_TASKS.tgz ./
+
+  kill -9 $SCHEDULER_PID
 
   # Restore stdout and stderr file descriptors.
   exec 1>&3 2>&4
+
+  printf "\n" >&3
 done
